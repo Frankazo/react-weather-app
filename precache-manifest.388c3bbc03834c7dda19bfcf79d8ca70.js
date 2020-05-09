9f8c2a497fe41328bfcf33a1bc285a2b382a95b2
Frankluisravlo@gmail.com <Frankluisravlo@gmail.com>
2020-05-09 11:00:21 -0400

    Updates

---
+++ precache-manifest.388c3bbc03834c7dda19bfcf79d8ca70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299a322cfdabce8933e420b1626ecfa0",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "75471171cd12087a669a",
+    "url": "/react-weather-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e23dd70aea4660364883",
+    "url": "/react-weather-app/static/css/main.1af8d8f7.chunk.css"
+  },
+  {
+    "revision": "75471171cd12087a669a",
+    "url": "/react-weather-app/static/js/2.40393e1d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-weather-app/static/js/2.40393e1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e23dd70aea4660364883",
+    "url": "/react-weather-app/static/js/main.18c127db.chunk.js"
+  },
+  {
+    "revision": "381d595b8f67db289ae3",
+    "url": "/react-weather-app/static/js/runtime-main.9485360c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.185ceaa79152bae9aa9cc17699d67c42.js"
+  "/react-weather-app/precache-manifest.388c3bbc03834c7dda19bfcf79d8ca70.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-weather-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
